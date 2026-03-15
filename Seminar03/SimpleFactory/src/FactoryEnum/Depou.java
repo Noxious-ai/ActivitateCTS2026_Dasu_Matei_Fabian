a0826065499b818bb1a9a8e1bdc4c08d68e8c4cf
Noxious-ai <mateidasu2@gmail.com>
2026-03-15 23:11:33 +0200

    Modificare Depou din simple factory in singleton simple factory

--- Seminar03/SimpleFactory/src/FactoryEnum/Depou.java
+++ Seminar03/SimpleFactory/src/FactoryEnum/Depou.java
@@ -6,14 +6,37 @@ import clase.Tramvai;
 import clase.Troleibuz;
 
 public class Depou {
-    public MijlocTransport getMijlocDeTransport(TipuriMijloaceTransport tip, int numarRoti, String numarInmatriculare){
-        if (tip == TipuriMijloaceTransport.TRAMVAI){
-            return new Tramvai(numarRoti, numarInmatriculare);
-        } else if (tip == TipuriMijloaceTransport.TROLEIBUZ) {
-            return new Troleibuz(numarRoti, numarInmatriculare);
-        } else if (tip == TipuriMijloaceTransport.AUTOBUZ) {
-            return new Autobuz(numarRoti, numarInmatriculare);
+    private static Depou instance = null;
+    private int numarVehicule = 0;
+
+    private Depou(){
+    }
+
+    public static synchronized Depou getInstance(){
+        if(instance == null){
+            instance = new Depou();
         }
-        return null;
+        return instance;
+    }
+
+    public MijlocTransport getMijlocDeTransport(TipuriMijloaceTransport tip, int numarRoti, String numarInmatriculare){
+        MijlocTransport mijloc = null;
+
+        if (tip == TipuriMijloaceTransport.TRAMVAI){
+            mijloc = new Tramvai(numarRoti, numarInmatriculare);
+        } else if (tip == TipuriMijloaceTransport.TROLEIBUZ) {
+            mijloc = new Troleibuz(numarRoti, numarInmatriculare);
+        } else if (tip == TipuriMijloaceTransport.AUTOBUZ) {
+            mijloc = new Autobuz(numarRoti, numarInmatriculare);
+        }
+        if (mijloc != null){
+            numarVehicule++;
+        }
+
+        return mijloc;
+    }
+
+    public void afiseazaNumarVehicule(){
+        System.out.println("Numarul de vehicule creeat este: " + numarVehicule);
     }
 }
--- Seminar03/SimpleFactory/src/main/Main.java
+++ Seminar03/SimpleFactory/src/main/Main.java
@@ -6,9 +6,18 @@ import clase.MijlocTransport;
 
 public class Main {
     public static void main(String[] args) {
-        Depou depou = new Depou();
+        Depou depou =  Depou.getInstance();
         MijlocTransport autobuz = depou.getMijlocDeTransport(TipuriMijloaceTransport.AUTOBUZ, 8, "B666XXX");
         MijlocTransport troleibuz = depou.getMijlocDeTransport(TipuriMijloaceTransport.TROLEIBUZ, 10, "B555YYY");
         MijlocTransport tramvai = depou.getMijlocDeTransport(TipuriMijloaceTransport.TRAMVAI, 10, "B888ZZZ");
+
+        depou.afiseazaNumarVehicule();
+
+        Depou depou1 = Depou.getInstance();
+
+        MijlocTransport autobuz2 = depou.getMijlocDeTransport(TipuriMijloaceTransport.AUTOBUZ, 10, "TM111AAA");
+
+        depou.afiseazaNumarVehicule();
+
     }
 }
